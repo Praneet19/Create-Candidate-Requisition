bf5302e1c23f1aa294471d35dd83b490b62b8afa
Praneet19 <niranjanpraneet19@gmail.com>
2022-03-27 21:02:24 +0530

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+import { ChakraProvider, Box, Container } from '@chakra-ui/react'
+import theme from './theme';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Multi-Step Form</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ChakraProvider theme={theme}>
+
+        <Component {...pageProps} />
+          
+        
+      </ChakraProvider>
+    </React.Fragment>
+  );
+}
---
+++ src/pages/theme.tsx
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1536px',
+})
+
+export default theme
